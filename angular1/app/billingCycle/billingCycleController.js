aef1e27e3ae076802345c066b64ccbaecf40116e
Leonardo <leocardosoti@gmail.com>
2017-04-27 15:01:26 -0300

    commit

---
+++ angular1/app/billingCycle/billingCycleController.js
@@ -0,0 +1,24 @@
+(function () {
+    angular.module('primeiraApp').controller('BillingCycleCtrl', [
+        '$http',
+        '$location',
+        'msgs',
+        'tabs',
+        BillingCycleController
+    ])
+
+    function BillingCycleController($http) {
+        const vm = this
+        const url = `http://localhost:3003/api/billingCycles`
+
+        vm.create = function () {
+
+            $http.post(url, vm.BillingCycle).then(function (response) {
+                console.log("Sucesso")
+                msgs.addSuccess('Operação realizada com sucesso!!')
+            }).catch(function (response) {
+                msgs.addError(response.data.errors)
+            })
+        }
+    }
+})()
