629228b584e1ce8ad81e3d3554729a55f56f6de1
Leonardo <leocardosoti@gmail.com>
2017-05-02 13:46:46 -0300

    correções BillingCycle

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -2,22 +2,22 @@
     angular.module('primeiraApp').controller('BillingCycleCtrl', [
         '$http',
         '$location',
-        'msgs',
-        'tabs',
+        //   'msgs',
+        //   'tabs',
         BillingCycleController
     ])
 
     function BillingCycleController($http) {
         const vm = this
         const url = `http://localhost:3003/api/billingCycles`
-
         vm.create = function () {
 
+            console.log(vm.BillingCycle)
             $http.post(url, vm.BillingCycle).then(function (response) {
                 console.log("Sucesso")
-                msgs.addSuccess('Operação realizada com sucesso!!')
+                //  msgs.addSuccess('Operação realizada com sucesso!!')
             }).catch(function (response) {
-                msgs.addError(response.data.errors)
+                //    msgs.addError(response.data.errors)
             })
         }
     }
